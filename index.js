162fbdb73c1953c91a7ef97e74e5c422187c993e
chandan-mandi <itscoderchandan@gmail.com>
2022-01-09 12:23:01 +0530

    env file update

--- index.js
+++ index.js
@@ -14,10 +14,15 @@ app.use(cors());
 app.use(express.json());
 
 
-const serviceAccount = JSON.parse(`${process.env.FIREBASE_SERVICE_ACCOUNT}`)
+// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL, 
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  }),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 
 
